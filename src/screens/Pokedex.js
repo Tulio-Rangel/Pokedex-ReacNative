f0b2a22031a26cfea3f6bce2bb8f86e62765feb1
Tulio-Rangel <tjrn20@hotmail.com>
2023-03-29 15:50:43 -0500

    Consumo de la poke api, info de 20 pokemon

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,24 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPokemonApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemon();
+    })();
+  }, []);
+
+  const loadPokemon = async () => {
+    try {
+      const response = await getPokemonApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
