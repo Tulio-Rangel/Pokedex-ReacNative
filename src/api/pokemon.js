743b56e173b6ddfc4b1632f6db5e873c2f3d5789
Tulio-Rangel <tjrn20@hotmail.com>
2023-03-29 17:24:46 -0500

    consumiendo detalles de los pokemon

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -10,3 +10,13 @@ export async function getPokemonApi() {
     throw error;
   }
 }
+
+export async function getPokemonDetailsByUrlApi(url) {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,9 +1,11 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useEffect, useState } from "react";
-import { getPokemonApi } from "../api/pokemon";
+import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
+  const [pokemon, setPokemon] = useState([]);
+  console.log("Pokemon--->", pokemon);
   useEffect(() => {
     (async () => {
       await loadPokemon();
@@ -13,7 +15,27 @@ export default function Pokedex() {
   const loadPokemon = async () => {
     try {
       const response = await getPokemonApi();
-      console.log(response);
+      const pokemonArray = [];
+
+      //Ejecutar en bucle async el array que me devuelve la api
+      //Es response.result porque el el response que estamos recibiendo de getPokemonApi, la propiedad result es la que trae el array de pokemon
+      for await (const iPokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(iPokemon.url);
+        pokemonArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          types: [
+            pokemonDetails.types[0].type.name,
+            pokemonDetails.types.length > 1
+              ? pokemonDetails.types[1].type.name
+              : null,
+          ],
+          order: pokemonDetails.order,
+          imagen:
+            pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+      setPokemon([...pokemon, ...pokemonArray]);
     } catch (error) {
       console.error(error);
     }
