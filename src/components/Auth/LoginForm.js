f0c6f6bd7765e6386f27c224efc6ce62afb585ed
Tulio-Rangel <tjrn20@hotmail.com>
2023-04-10 18:03:31 -0500

    formulario login

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,10 +1,46 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
 import React from "react";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Log in</Text>
+      <TextInput
+        placeholder="Username"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title="Enter" onPress={() => console.log("Entrando...")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
