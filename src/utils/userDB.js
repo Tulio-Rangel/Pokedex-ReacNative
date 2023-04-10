23eb1e28f8eea3394ea05e1ef0ea163bbb99c057
Tulio-Rangel <tjrn20@hotmail.com>
2023-04-10 17:41:23 -0500

    datos mockeados de usuario

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: "ash",
+  password: "mostaza",
+};
+
+export const userDetails = {
+  usename: "ash",
+  firstName: "Ash",
+  lastname: "Ketchum",
+  email: "campeon2023@gmail.com",
+};
