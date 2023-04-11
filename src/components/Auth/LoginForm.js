7af680ce99f5810a33457e7a9323a58252bd1705
Tulio-Rangel <tjrn20@hotmail.com>
2023-04-10 19:12:02 -0500

    Validaciones de formularios con Yup y Formik

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -7,8 +7,19 @@ import {
   Keyboard,
 } from "react-native";
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup"; //Este es el sistema de validacion
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    //validationOnChange: false //Para que la validacion no sea en tiempo real sino cuando se le de submit
+    onSubmit: (formValueEntered) => {
+      console.log(formValueEntered);
+    },
+  });
+
   return (
     <View>
       <Text style={styles.title}>Log in</Text>
@@ -16,16 +27,36 @@ export default function LoginForm() {
         placeholder="Username"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
       />
       <TextInput
         placeholder="Password"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
       />
-      <Button title="Enter" onPress={() => console.log("Entrando...")} />
+      <Button title="Enter" onPress={formik.handleSubmit} />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
+}
+
+function initialValues() {
+  return {
+    username: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().required("Password is required"),
+  };
 }
 
 const styles = StyleSheet.create({
@@ -42,5 +73,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+  },
+  error: {
+    textAlign: "center",
+    color: "#f00",
+    marginTop: 20,
   },
 });
