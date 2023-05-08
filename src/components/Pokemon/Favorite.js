6738fe047f5773357441f595d8c0884b9cfca360
Tulio-Rangel <tjrn20@hotmail.com>
2023-05-08 10:42:10 -0500

    add function to fav, get favorites isnt working yet

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import { addPokemonFavorite } from "../../api/favorite";
 
 export default function Favorite(props) {
   const { id } = props;
-  const addFavorite = () => {
-    console.log("Added to favorires", id);
+  const addFavorite = async () => {
+    await addPokemonFavorite(id);
   };
+
   return (
     <Icon
       name="heart"
--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,11 +1,18 @@
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import { getPokemonsFavoriteApi } from "../api/favorite";
 
 export default function Favorites() {
+  const checkFavorites = async () => {
+    const response = await getPokemonsFavoriteApi();
+    console.log(response);
+  };
+
   return (
     <SafeAreaView>
       <Text>Favorites</Text>
+      <Button title="Obtener favorito" onPress={checkFavorites} />
     </SafeAreaView>
   );
 }
