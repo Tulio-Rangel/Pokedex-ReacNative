c079dd472ac6f39a1c4e5571a415d518eda582b4
Tulio-Rangel <tjrn20@hotmail.com>
2023-05-08 11:51:36 -0500

    pagina de lista de favoritos

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,18 +1,50 @@
 import { Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { getPokemonsFavoriteApi, isPokemonFavoriteApi } from "../api/favorite";
+import { getPokemonDetailApi } from "../api/pokemon";
+import useAuth from "../hooks/useAuth";
+import PokemonList from "../components/PokemonList";
 
 export default function Favorites() {
-  const checkFavorites = async () => {
-    const response = await getPokemonsFavoriteApi();
-    console.log(response);
-  };
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
 
-  return (
-    <SafeAreaView>
-      <Text>Favorites</Text>
-      <Button title="Obtener favorito" onPress={checkFavorites} />
-    </SafeAreaView>
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = getPokemonsFavoriteApi();
+
+          const pokemonsArray = [];
+
+          for await (const id of response) {
+            const pokemonDetails = await getPokemonDetailApi(id);
+            pokemonsArray.push({
+              id: pokemonDetails.id,
+              name: pokemonDetails.name,
+              types: [
+                pokemonDetails.types[0].type.name,
+                pokemonDetails.types.length > 1
+                  ? pokemonDetails.types[1].type.name
+                  : null,
+              ],
+              order: pokemonDetails.order,
+              image:
+                pokemonDetails.sprites.other["official-artwork"].front_default,
+            });
+          }
+
+          setPokemons(pokemonsArray);
+        })();
+      }
+    }, [auth])
+  );
+
+  return !auth ? (
+    <Text>Usuario no logeado</Text>
+  ) : (
+    <PokemonList pokemons={pokemons} />
   );
 }
