baaf1e4ae1ed7f4ece0e7091a27ab84eaa786f97
Tulio-Rangel <tjrn20@hotmail.com>
2023-05-08 12:03:19 -0500

    navegación hacia atrás de favoritos

--- src/navigation/FavoriteNavigation.js
+++ src/navigation/FavoriteNavigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Favorites from "../screens/Favorites";
+import Pokemon from "../screens/Pokedex";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,14 @@ export default function FavoriteNavigation() {
         name="Favorites"
         component={Favorites}
         options={{ headerTitleAlign: "center" }}
+      />
+      <Stack.Screen
+        name="Pokemon"
+        component={Pokemon}
+        options={{
+          title: "",
+          headerTransparent: true,
+        }}
       />
     </Stack.Navigator>
   );
